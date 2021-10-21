a3fc675dde0253ed2638478fab0325cba462bfa6
GitHub Action <action@github.com>
2021-10-21 16:18:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/vovahost/suppressfolding/MyBundle.kt
+++ src/main/kotlin/com/github/vovahost/suppressfolding/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vovahost.suppressfolding
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/vovahost/suppressfolding/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vovahost/suppressfolding/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vovahost.suppressfolding.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vovahost.suppressfolding.MyBundle
 
 class MyApplicationService {
 
